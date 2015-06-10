a8c9a3f92964369a4edab008de9fc02723b667ce
steven harradine <steven.harradine@telus.com>
2015-06-10 01:26:05 -0400

    add media styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,9 @@ module.exports = function(grunt) {
 					'www/css/fonts.css' : 'www/css/fonts.scss',
 
 					'www/apps/**/css/*.css' : 'www/apps/**/css/*.scss',
-                                        'www/apps/SARAH-reader/css/styles.css' : 'www/apps/SARAH-reader/css/styles.scss'
+                    'www/apps/SARAH-reader/css/styles.css' : 'www/apps/SARAH-reader/css/styles.scss',
+                    'www/apps/SARAH-media/css/styles.css' : 'www/apps/SARAH-media/css/styles.scss',
+                    'www/apps/SARAH-media/css/player.css' : 'www/apps/SARAH-media/css/player.scss'
 
 				}
 			}
