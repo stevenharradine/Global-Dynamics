bb0bd99c28862359adad9786fb08feeee273f664
steven harradine <steven.harradine@telus.com>
2015-06-18 00:10:27 -0400

    remove db initialization in php

--- www/global.php
+++ www/global.php
@@ -24,31 +24,6 @@
 		return isset ($_REQUEST[$index]) ? $_REQUEST[$index] : $default_value;
 	}
 	
-	function isDBInit () {
-		$sql = <<<EOD
-	SELECT
-		COUNT(*)
-	FROM
-		INFORMATION_SCHEMA.SCHEMATA
-	WHERE
-		SCHEMA_NAME = 'sarah'
-EOD;
-		$db_list = mysql_query ( $sql ) or die (mysql_error());
-
-		return mysql_fetch_array($db_list)['COUNT(*)'];
-	}
-	
-	function DBInit () {
-		$createDB					= mysql_query ('CREATE DATABASE `sarah`') or die (mysql_error()) ? 'Created database "sarah"' : 'ERROR: database "sarah" not created';
-		$insertUsers				= mysql_query ("INSERT INTO `sarah`.`users` (`USER_ID`, `user_type`, `username`, `hash_algorithm`, `salt`, `password`) VALUES (1, 'ADMIN', 'douglas', 'SHA256', 'HjgawkNjtbanNZ8KBx3Er9xPRjHaGLCXSGExofDVQhBT1uGbK7kZSr+oC+RtZqnDN79XH228wxsJi8+R0gat/g==', '51bee9acdef40861896bfeb04cc3c6a3f0ba6392a2500e77e84949f3cea6526c');") or die (mysql_error());
-		$insertSettings				= mysql_query ("INSERT INTO `sarah`.`settings` (`key`, `value`) VALUES ('music_fileRoot', '/Music'), ('music_localStartPath', '/etc/SARAH/media'), ('music_webStartPath', 'http://localhost/media'), ('music_flacTranscodePath', '/transcode/flac'), ('music_coverIndex', '0'), ('music_coversPath', '/covers'), ('youtubedownloader_savedir', '/Music/Music%20Videos/'), ('music_audio', 'mp3;flac;aac;m4a'), ('music_video', 'mp4;flv;mpg;mpeg;mkv;ts;avi'), ('music_default_audio', 'mp3'), ('music_default_video', 'mp4');") or die (mysql_error());
-
-		return	$createDB &&
-				$insertSettings &&
-				$insertUsers;
-		//echo mysql_query ("") or die (mysql_error());
-	}
-	
 	function sEditWrite ($id, $label, $value, $extra = '') {
 		return "<form action='index.php' method='POST'>$extra<input type='hidden' name='id' value='$id' /><input type='hidden' name='action' value='save_$label". "_by_id' /><input type='text' value='$value' name='$label' /><input type='submit' value='Save' /></form>";
 	}
