0e9bfaa5416537825df0614b0869f25b99b30d62
Steven Harradine <stevenharradine@gmail.com>
2015-06-20 22:37:01 -0400

    remove wildcard css compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
 					'www/css/responsive.css' : 'www/css/responsive.scss',
 					'www/css/fonts.css' : 'www/css/fonts.scss',
 
-					'www/apps/**/css/*.css' : 'www/apps/**/css/*.scss',
+//					'www/apps/**/css/*.css' : 'www/apps/**/css/*.scss',
                     'www/apps/SARAH-reader/css/styles.css' : 'www/apps/SARAH-reader/css/styles.scss',
                     'www/apps/SARAH-media/css/styles.css' : 'www/apps/SARAH-media/css/styles.scss',
                     'www/apps/SARAH-media/css/player.css' : 'www/apps/SARAH-media/css/player.scss',
